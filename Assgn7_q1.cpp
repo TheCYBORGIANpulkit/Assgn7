e50232e125d8fda5883250e3786360e054b6f827
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2020-12-06 22:15:38 +0530

    Add files via upload

---
+++ Assgn7_q1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+#include "Library.cpp"
+using namespace std;
+
+float f(float x, float y){
+    float z = y*(log(y))/x;
+    return z;
+}
+
+int main(){
+    int N = 100;
+    float xi = 2;
+    float xf = 10;
+    float y = 2.71828;
+    Explicit_Euler(f,xi,10,y,200);
+    return 0;
+}
